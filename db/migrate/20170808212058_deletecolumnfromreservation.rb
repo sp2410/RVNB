d2547c2964475c9fd886cf10555f4e3e8bd41a73
saprakas@syr.edu <saprakas@syr.edu>
2017-09-07 18:11:56 -0700

    status precompile

--- db/migrate/20170808212058_deletecolumnfromreservation.rb
+++ db/migrate/20170808212058_deletecolumnfromreservation.rb
@@ -1,7 +1,7 @@
 class Deletecolumnfromreservation < ActiveRecord::Migration[5.1]
   def change
   	remove_column :reservations, :startdate
-  	remove_column :reservations, :enddate
+  	# remove_column :reservations, :enddate
   	remove_column :reservations, :needcaptain
   	
   end
